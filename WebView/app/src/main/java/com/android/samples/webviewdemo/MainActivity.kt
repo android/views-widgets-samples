1ac2340959687523b1211adc97935df4c3a89aeb
Grace Coleman <grcoleman@google.com>
2020-06-16 17:59:37 -0400

    reformats and adds headers

--- WebView/app/src/main/java/com/android/samples/webviewdemo/MainActivity.kt
+++ WebView/app/src/main/java/com/android/samples/webviewdemo/MainActivity.kt
@@ -18,43 +18,32 @@ package com.android.samples.webviewdemo
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.JavascriptInterface
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
-import android.widget.Toast
 import androidx.core.content.ContextCompat.startActivity
-import androidx.webkit.*
-import androidx.webkit.R
+import androidx.webkit.WebViewAssetLoader
+import androidx.webkit.WebViewClientCompat
 import com.android.samples.webviewdemo.databinding.ActivityMainBinding
-import org.json.JSONObject
-import java.io.ByteArrayInputStream
-import java.io.FileInputStream
+
 
 class MainActivity : AppCompatActivity() {
-
     //Creating the custom WebView Client Class
     private class MyWebViewClient(private val assetLoader: WebViewAssetLoader) :
         WebViewClientCompat() {
-
         override fun shouldInterceptRequest(
             view: WebView,
             request: WebResourceRequest
         ): WebResourceResponse? {
-
             return assetLoader.shouldInterceptRequest(request.getUrl());
-
         }
-
     }
 
     /** Instantiate the interface and set the context  */
     class WebAppInterface(private val mContext: Context) {
-
-
         /** Send a message from the web page  */
         @JavascriptInterface
         fun sendMessage(message: String) {
@@ -63,21 +52,15 @@ class MainActivity : AppCompatActivity() {
                 putExtra(Intent.EXTRA_TEXT, message)
                 type = "text/plain"
             }
-
             val shareIntent = Intent.createChooser(sendIntent, null)
             startActivity(mContext, shareIntent, null)
-
-
         }
-
-
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
 
         //Configure asset loader with custom domain
         val assetLoader = WebViewAssetLoader.Builder()
@@ -98,10 +81,7 @@ class MainActivity : AppCompatActivity() {
         //Connect to Javascript Interface
         binding.webview.addJavascriptInterface(WebAppInterface(this), "Weather")
 
+        //Load the content
         binding.webview.loadUrl("https://gcoleman799.github.io/Asset-Loader/index.html")
-
-
     }
-
-
 }
