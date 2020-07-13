ecfd6d651db3d2b322738dce8c1ccec81da82953
Grace Coleman <grcoleman@google.com>
2020-07-13 19:21:22 -0400

    adds force dark compatability

--- WebView/app/src/main/java/com/android/samples/webviewdemo/MainActivity.kt
+++ WebView/app/src/main/java/com/android/samples/webviewdemo/MainActivity.kt
@@ -16,8 +16,10 @@
 
 package com.android.samples.webviewdemo
 
+import android.content.Context
 import android.content.Intent
 import android.content.pm.ApplicationInfo
+import android.content.res.Configuration
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -28,9 +30,14 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import androidx.core.content.ContextCompat.startActivity
 import androidx.webkit.JavaScriptReplyProxy
 import androidx.webkit.WebMessageCompat
+import androidx.webkit.WebSettingsCompat
+import androidx.webkit.WebSettingsCompat.DARK_STRATEGY_USER_AGENT_DARKENING_ONLY
+import androidx.webkit.WebSettingsCompat.DARK_STRATEGY_WEB_THEME_DARKENING_ONLY
 import androidx.webkit.WebViewAssetLoader
 import androidx.webkit.WebViewClientCompat
 import androidx.webkit.WebViewCompat
@@ -111,7 +118,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     // Invokes native android sharing
-    private fun invokeShareIntent (message: String) {
+    private fun invokeShareIntent(message: String) {
         val sendIntent: Intent = Intent().apply {
             action = Intent.ACTION_SEND
             putExtra(Intent.EXTRA_TEXT, message)
@@ -127,6 +134,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         val jsObjName = "jsObject"
         val allowedOriginRules = setOf<String>("https://gcoleman799.github.io")
+
+        // Check if the system is set to light or dark mode
+        val nightModeFlag1 = resources.configuration.uiMode
+        val nightModeFlag2 = Configuration.UI_MODE_NIGHT_MASK
+        if (nightModeFlag1.and(nightModeFlag2) == Configuration.UI_MODE_NIGHT_YES) {
+            // Switch WebView to dark mode; uses default dark theme
+            if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+                WebSettingsCompat.setForceDark(
+                    binding.webview.settings,
+                    WebSettingsCompat.FORCE_DARK_ON
+                )
+            }
+        }
 
         // Configure asset loader with custom domain
         // * NOTE THAT *:
@@ -164,7 +184,7 @@ class MainActivity : AppCompatActivity() {
             binding.webview,
             jsObjName,
             allowedOriginRules
-        ) { message ->invokeShareIntent(message)}
+        ) { message -> invokeShareIntent(message) }
 
         // Load the content
         binding.webview.loadUrl("https://gcoleman799.github.io/Asset-Loader/assets/index.html")
