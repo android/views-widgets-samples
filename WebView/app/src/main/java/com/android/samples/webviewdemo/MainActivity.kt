7c3b2c12d86c990dd7513dab46b6fafdfeaecf4d
Grace Coleman <grcoleman@google.com>
2020-07-22 14:53:56 -0400

    add comment about default reloading

--- WebView/app/src/main/java/com/android/samples/webviewdemo/MainActivity.kt
+++ WebView/app/src/main/java/com/android/samples/webviewdemo/MainActivity.kt
@@ -131,9 +131,15 @@ class MainActivity : AppCompatActivity() {
         val jsObjName = "jsObject"
         val allowedOriginRules = setOf<String>("https://gcoleman799.github.io")
 
-        // FORCE DARK
-        // Check if the system is set to light or dark mode
+        // Configuring Dark Theme
+        // *NOTE* : The force dark setting is not persistent. You must call the static
+        // method every time your app process is started.
+        // *NOTE* : The change from day<->night mode is a
+        // configuration change so by default the activity will be restarted
+        // (and pickup the new values to apply the theme). Take care when overriding this
+        //  default behavior to ensure this method is still called when changes are made.
         val nightModeFlag = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        // Check if the system is set to light or dark mode
         if (nightModeFlag == Configuration.UI_MODE_NIGHT_YES) {
             // Switch WebView to dark mode; uses default dark theme
             if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
@@ -160,53 +166,52 @@ class MainActivity : AppCompatActivity() {
                     DARK_STRATEGY_PREFER_WEB_THEME_OVER_USER_AGENT_DARKENING
                 )
             }
-
-            // Configure asset loader with custom domain
-            // * NOTE THAT *:
-            // The assets path handler is set with the sub path /views-widgets-samples/ here because we are tyring to ensure
-            // that the address loaded with loadUrl("https://raw.githubusercontent.com/views-widgets-samples/assets/index.html") does
-            // not conflict with a real web address. In this case, if the path were only /assests/ we would need to load
-            // "https://raw.githubusercontent.com/assets/index.html" in order to access our local index.html file.
-            // However we cannot guarantee "https://raw.githubusercontent.com/assets/index.html" is not a valid web address.
-            // Therefore we must let the AssetLoader know to expect the /views-widgets-samples/ sub path as well as the /assets/.
-            val assetLoader = WebViewAssetLoader.Builder()
-                .setDomain("raw.githubusercontent.com")
-                .addPathHandler(
-                    "/views-widgets-samples/assets/",
-                    WebViewAssetLoader.AssetsPathHandler(this)
-                )
-                .addPathHandler(
-                    "/views-widgets-samples/res/",
-                    WebViewAssetLoader.ResourcesPathHandler(this)
-                )
-                .build()
-
-            // Set clients
-            binding.webview.webViewClient = MyWebViewClient(assetLoader)
-
-            // Set Title
-            title = getString(R.string.app_name)
-
-            // Setup debugging; See https://developers.google.com/web/tools/chrome-devtools/remote-debugging/webviews for reference
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                if (0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) {
-                    WebView.setWebContentsDebuggingEnabled(true)
-                }
-            }
-
-            // Enable Javascript
-            binding.webview.settings.javaScriptEnabled = true
-
-            // Create a JS object to be injected into frames; Determines if WebMessageListener
-            // or WebAppInterface should be used
-            createJsObject(
-                binding.webview,
-                jsObjName,
-                allowedOriginRules
-            ) { message -> invokeShareIntent(message) }
-
-            // Load the content
-            binding.webview.loadUrl("https://raw.githubusercontent.com/views-widgets-samples/assets/index.html")
         }
-    }
+
+        // Configure asset loader with custom domain
+        // *NOTE* :
+        // The assets path handler is set with the sub path /views-widgets-samples/ here because we are tyring to ensure
+        // that the address loaded with loadUrl("https://raw.githubusercontent.com/views-widgets-samples/assets/index.html") does
+        // not conflict with a real web address. In this case, if the path were only /assests/ we would need to load
+        // "https://raw.githubusercontent.com/assets/index.html" in order to access our local index.html file.
+        // However we cannot guarantee "https://raw.githubusercontent.com/assets/index.html" is not a valid web address.
+        // Therefore we must let the AssetLoader know to expect the /views-widgets-samples/ sub path as well as the /assets/.
+        val assetLoader = WebViewAssetLoader.Builder()
+            .setDomain("raw.githubusercontent.com")
+            .addPathHandler(
+                "/views-widgets-samples/assets/",
+                WebViewAssetLoader.AssetsPathHandler(this)
+            )
+            .addPathHandler(
+                "/views-widgets-samples/res/",
+                WebViewAssetLoader.ResourcesPathHandler(this)
+            )
+            .build()
+
+        // Set clients
+        binding.webview.webViewClient = MyWebViewClient(assetLoader)
+
+        // Set Title
+        title = getString(R.string.app_name)
+
+        // Setup debugging; See https://developers.google.com/web/tools/chrome-devtools/remote-debugging/webviews for reference
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) {
+                WebView.setWebContentsDebuggingEnabled(true)
+            }
+        }
+
+        // Enable Javascript
+        binding.webview.settings.javaScriptEnabled = true
+
+        // Create a JS object to be injected into frames; Determines if WebMessageListener
+        // or WebAppInterface should be used
+        createJsObject(
+            binding.webview,
+            jsObjName,
+            allowedOriginRules
+        ) { message -> invokeShareIntent(message) }
+
+        // Load the content
+        binding.webview.loadUrl("https://raw.githubusercontent.com/views-widgets-samples/assets/index.html")    }
 }
