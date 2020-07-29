e72c633ca2b8c5b57215c35ddf5a034a4cba14bd
Sean McQuillan <seanmcq@google.com>
2020-07-29 15:22:50 -0700

    Bump to beta8 and try out new saved instance state API.

--- ConstraintLayoutExamples/motionlayoutintegrations/src/main/java/com/example/androidstudio/motionlayoutintegrations/Entrance.kt
+++ ConstraintLayoutExamples/motionlayoutintegrations/src/main/java/com/example/androidstudio/motionlayoutintegrations/Entrance.kt
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.androidstudio.motionlayoutintegrations.databinding.ActivityEntranceBinding
 
-private val SIS_MOTION_LAYOUT_START_STATE: String = "sis_motion_layout_start"
-private val SIS_MOTION_LAYOUT_END_STATE: String = "sis_motion_layout_end"
-private val SIS_MOTION_LAYOUT_PROGRESS = "sis_motion_layout_progress"
+private val SIS_MOTION_LAYOUT_STATE: String = "sis_motion_layout_start"
 
 class Entrance : AppCompatActivity() {
 
@@ -18,23 +16,12 @@ class Entrance : AppCompatActivity() {
         setContentView(binding.root)
 
         savedInstanceState?.let {
-            binding.motionLayout.setTransition(
-                savedInstanceState.getInt(SIS_MOTION_LAYOUT_START_STATE),
-                savedInstanceState.getInt(SIS_MOTION_LAYOUT_END_STATE)
-            )
-
-            binding.motionLayout.progress = savedInstanceState.getFloat(SIS_MOTION_LAYOUT_PROGRESS)
+            binding.motionLayout.transitionState = savedInstanceState.getBundle(SIS_MOTION_LAYOUT_STATE)
         }
-
-        // Kick off the first transition, or the restored transition
-        binding.motionLayout.transitionToEnd()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-
-        outState.putInt(SIS_MOTION_LAYOUT_START_STATE, binding.motionLayout.startState)
-        outState.putInt(SIS_MOTION_LAYOUT_END_STATE, binding.motionLayout.endState)
-        outState.putFloat(SIS_MOTION_LAYOUT_PROGRESS, binding.motionLayout.progress)
+        outState.putBundle(SIS_MOTION_LAYOUT_STATE, binding.motionLayout.transitionState)
     }
 }
