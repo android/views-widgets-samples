9d74b74942da80421454eb395062ed91acb7bcae
Stylianos Gakis <stylianos.gakis98@gmail.com>
2021-10-16 11:17:23 +0200

    Fix DiffUtil.ItemCallback implementation

--- RecyclerViewKotlin/app/src/main/java/com/example/recyclersample/flowerList/FlowersAdapter.kt
+++ RecyclerViewKotlin/app/src/main/java/com/example/recyclersample/flowerList/FlowersAdapter.kt
@@ -75,10 +75,10 @@ class FlowersAdapter(private val onClick: (Flower) -> Unit) :
 
 object FlowerDiffCallback : DiffUtil.ItemCallback<Flower>() {
     override fun areItemsTheSame(oldItem: Flower, newItem: Flower): Boolean {
-        return oldItem == newItem
+        return oldItem.id == newItem.id
     }
 
     override fun areContentsTheSame(oldItem: Flower, newItem: Flower): Boolean {
-        return oldItem.id == newItem.id
+        return oldItem == newItem
     }
 }
