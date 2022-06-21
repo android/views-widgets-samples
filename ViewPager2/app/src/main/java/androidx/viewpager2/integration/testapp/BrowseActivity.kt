ee9a3acee1b86c7a91940141c7167c8e192f5bea
Alan Chiou <ce173310@gmail.com>
2022-06-21 23:53:38 +0800

    Clean and upgrade dependencies
    Gradle: 7.3.3
    Android Gradle Plugin: 7.2.1
    Kotlin: 1.7.0
    Other dependencies with stable version
    Add missing dependencies
    
    Test: build successfully

--- ViewPager2/app/src/main/java/androidx/viewpager2/integration/testapp/BrowseActivity.kt
+++ ViewPager2/app/src/main/java/androidx/viewpager2/integration/testapp/BrowseActivity.kt
@@ -16,59 +16,92 @@
 
 package androidx.viewpager2.integration.testapp
 
-import android.app.ListActivity
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.ListView
-import android.widget.SimpleAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * This activity lists all the activities in this application.
  */
-class BrowseActivity : ListActivity() {
+class BrowseActivity : Activity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browse)
 
-        listAdapter = SimpleAdapter(this, getData(),
-                android.R.layout.simple_list_item_1, arrayOf("title"),
-                intArrayOf(android.R.id.text1))
+        with(findViewById<RecyclerView>(R.id.list)) {
+            layoutManager = LinearLayoutManager(this@BrowseActivity)
+            adapter = BrowseAdapter(data = getData()) { map ->
+                val intent = Intent(map["intent"] as Intent)
+                intent.addCategory(Intent.CATEGORY_SAMPLE_CODE)
+                startActivity(intent)
+            }
+        }
     }
 
     private fun getData(): List<Map<String, Any>> {
-        val myData = mutableListOf<Map<String, Any>>()
+        val data = mutableListOf<Map<String, Any>>()
 
-        myData.add(mapOf("title" to "ViewPager2 with Views",
-                "intent" to activityToIntent(CardViewActivity::class.java.name)))
-        myData.add(mapOf("title" to "ViewPager2 with Fragments",
-                "intent" to activityToIntent(CardFragmentActivity::class.java.name)))
-        myData.add(mapOf("title" to "ViewPager2 with a Mutable Collection (Views)",
-                "intent" to activityToIntent(MutableCollectionViewActivity::class.java.name)))
-        myData.add(mapOf("title" to "ViewPager2 with a Mutable Collection (Fragments)",
-                "intent" to activityToIntent(MutableCollectionFragmentActivity::class.java.name)))
-        myData.add(mapOf("title" to "ViewPager2 with a TabLayout (Views)",
-                "intent" to activityToIntent(CardViewTabLayoutActivity::class.java.name)))
-        myData.add(mapOf("title" to "ViewPager2 with Fake Dragging",
-                "intent" to activityToIntent(FakeDragActivity::class.java.name)))
-        myData.add(mapOf("title" to "ViewPager2 with PageTransformers",
-                "intent" to activityToIntent(PageTransformerActivity::class.java.name)))
-        myData.add(mapOf("title" to "ViewPager2 with a Preview of Next/Prev Page",
-                "intent" to activityToIntent(PreviewPagesActivity::class.java.name)))
-        myData.add(mapOf("title" to "ViewPager2 with Nested RecyclerViews",
-                "intent" to activityToIntent(ParallelNestedScrollingActivity::class.java.name)))
+        data.add(
+            mapOf(
+                "title" to "ViewPager2 with Views",
+                "intent" to activityToIntent(CardViewActivity::class.java.name)
+            )
+        )
+        data.add(
+            mapOf(
+                "title" to "ViewPager2 with Fragments",
+                "intent" to activityToIntent(CardFragmentActivity::class.java.name)
+            )
+        )
+        data.add(
+            mapOf(
+                "title" to "ViewPager2 with a Mutable Collection (Views)",
+                "intent" to activityToIntent(MutableCollectionViewActivity::class.java.name)
+            )
+        )
+        data.add(
+            mapOf(
+                "title" to "ViewPager2 with a Mutable Collection (Fragments)",
+                "intent" to activityToIntent(MutableCollectionFragmentActivity::class.java.name)
+            )
+        )
+        data.add(
+            mapOf(
+                "title" to "ViewPager2 with a TabLayout (Views)",
+                "intent" to activityToIntent(CardViewTabLayoutActivity::class.java.name)
+            )
+        )
+        data.add(
+            mapOf(
+                "title" to "ViewPager2 with Fake Dragging",
+                "intent" to activityToIntent(FakeDragActivity::class.java.name)
+            )
+        )
+        data.add(
+            mapOf(
+                "title" to "ViewPager2 with PageTransformers",
+                "intent" to activityToIntent(PageTransformerActivity::class.java.name)
+            )
+        )
+        data.add(
+            mapOf(
+                "title" to "ViewPager2 with a Preview of Next/Prev Page",
+                "intent" to activityToIntent(PreviewPagesActivity::class.java.name)
+            )
+        )
+        data.add(
+            mapOf(
+                "title" to "ViewPager2 with Nested RecyclerViews",
+                "intent" to activityToIntent(ParallelNestedScrollingActivity::class.java.name)
+            )
+        )
 
-        return myData
+        return data
     }
 
     private fun activityToIntent(activity: String): Intent =
-            Intent(Intent.ACTION_VIEW).setClassName(this.packageName, activity)
-
-    override fun onListItemClick(listView: ListView, view: View, position: Int, id: Long) {
-        val map = listView.getItemAtPosition(position) as Map<*, *>
-
-        val intent = Intent(map["intent"] as Intent)
-        intent.addCategory(Intent.CATEGORY_SAMPLE_CODE)
-        startActivity(intent)
-    }
+        Intent(Intent.ACTION_VIEW).setClassName(this.packageName, activity)
 }
